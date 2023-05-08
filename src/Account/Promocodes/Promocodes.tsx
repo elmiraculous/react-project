abe866da844c1970f93cc748f61543668f79fa33
Elmira <gilfanova-03@inbox.ru>
2023-05-08 23:20:55 +0300

    добавлениt active

--- src/Account/Promocodes/Promocodes.tsx
+++ src/Account/Promocodes/Promocodes.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import images from "../../images/Promocodes/img.png"
+import images2 from "../../images/Promocodes/img_2.png"
 import styles from "./Promocodes.module.sass"
 
 
@@ -14,8 +15,15 @@ export const Promocodes = (props: PromocodesProps) => {
 	return (
 		<div className={styles.main}>
 			<div className={styles.main__img}>
-				<img className={styles.main__img__i} src={images} alt={"images"}/>
-				{/*<span className={styles.main__img__price}>{props.price}</span>*/}
+				{props.active ? (
+					<div>
+						<img className={styles.main__img__i} src={images} alt={"images"}/>
+					</div>
+				) : <div>
+					<img className={styles.main__img__i} src={images2} alt={"images"}/>
+				</div>
+				}
+				<span className={styles.main__img__price}>{props.price}</span>
 			</div>
 
 			<div className={styles.main__info}>
@@ -31,9 +39,12 @@ export const Promocodes = (props: PromocodesProps) => {
 					<span className={styles.main__info__link__l}>{props.link}</span>
 				</div>
 			</div>
-			<div>
-				<button className={styles.main__button}>Показать qr-код</button>
-			</div>
+			{props.active ? (
+				<div>
+					<button className={styles.main__button}>Показать qr-код</button>
+				</div>
+			) : null}
+
 		</div>
 	)
 }
